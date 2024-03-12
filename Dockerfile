fbd383cc6caab90ba33eb3b2aa35956a9e093e23
kenfidelis <efokpara@gmail.com>
2024-03-12 20:36:00 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ COPY . /app
 RUN python -m pip install pip==20.0.2
 RUN pip install -r /app/requirements.txt
 
+
 EXPOSE 5000
 
 CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "5000"]
--- init_standard.sh
+++ init_standard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kubectl apply -f clusterissuer.yaml
-
+kubectl create namespace standard
 kubectl apply -f config.yaml -n standard
 kubectl apply -f YAML-STANDARD/. -n standard
 kubectl get all -n standard
